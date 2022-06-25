5124a42cf62ebdad8ac4be8794aeb7b490deb5b2
LeRatGondin <44407018+LeRatGondin@users.noreply.github.com>
2022-06-25 11:10:07 +0200

    Update main.py

--- LoginToken/main.py
+++ LoginToken/main.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+import chromedriver_binary
 from selenium import webdriver
 import os
 def main(_token):
